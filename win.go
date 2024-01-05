fff3a944d7b7229cac3720769daf4ca843116130
abakum <koka.abakum@gmail.com>
2024-01-05 13:20:30 +0300

    ssh_host_ecdsa_key

--- win.go
+++ win.go
@@ -42,8 +42,8 @@ func GetHostKey(ssh string) (pri string) {
 		ssh,
 	} {
 		for _, key := range []string{
-			"ssh_host_ed25519_key",
 			"ssh_host_ecdsa_key",
+			"ssh_host_ed25519_key",
 			sshHostKey,
 		} {
 			pri = filepath.Join(dir, key)
